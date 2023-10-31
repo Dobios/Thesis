ce987fb5770e2564cd294d7fb414a09ce9023991
Dobios <adobis@student.ethz.ch>
2023-10-30 18:49:22 -0700

    Added a sequential circuit fir example

--- tests/firrtl-examples/chisel/inc.scala
+++ tests/firrtl-examples/chisel/inc.scala
@@ -3,8 +3,6 @@ import chisel3.stage.ChiselStage
 
 class Inc extends Module {
     val a = IO(Input(UInt(32.W)))
-    val en = IO(Input(Bool()))
-
     val b = a + 1.U
 
     assert(b > a, "a + 1 should always be bigger than a")
